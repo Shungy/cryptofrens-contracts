c5d02353abd28ef1d317a7f73fa570d415291036
Sunur Efe Vural <efe@efe.kim>
2022-03-03 20:43:52 -0500

    fix whitespace

--- contracts/Claimable.sol
+++ contracts/Claimable.sol
@@ -6,12 +6,12 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract Claimable is Ownable {
     address private _pendingOwner;
 
-		function claimOwnership() external {
-		    require(_msgSender() == _pendingOwner, "Claimable: not pending owner");
-		    _transferOwnership(_pendingOwner);
-		}
+    function claimOwnership() external {
+        require(_msgSender() == _pendingOwner, "Claimable: not pending owner");
+        _transferOwnership(_pendingOwner);
+    }
 
     function transferOwnership(address newOwner) public override onlyOwner {
-				_pendingOwner = newOwner;
+        _pendingOwner = newOwner;
     }
 }
